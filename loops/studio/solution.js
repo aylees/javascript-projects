855f671651af03b2743dc06b09cedf201ff62570
aylees <sutherlin.rylee@gmail.com>
2024-06-24 20:28:51 -0500

    Loops Studio

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -2,11 +2,12 @@ const input = require('readline-sync');
 
 // Part A: #1 Populate these arrays
 
-let protein = [];
-let grains = [];
-let veggies = [];
-let beverages = [];
-let desserts = [];
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grains = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let veggies = ['peas', 'green beans', 'kale', 'edamame', 'broccoli', 'asparagus'];
+let beverages = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'];
+
 
 
 function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
@@ -16,16 +17,26 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
 
+  for (let i = 0; i < numMeals; i++) {
+let meal = [];
+  for (let j = 0; j < pantry.length; j++) {
+ meal.push(pantry[j][i]);
 
-  return meals;
+meals.push(meal);
+ }
+
+ return meals;
 }
 
-
+}
 function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
   
   /// CODE YOUR SOLUTION TO PART B here ///
-
+  // Hi Team, This is a note from Rylee - we did not pass the last test. We did good overall so that's a win for us! //
+while (numMeals < 1 || numMeals > 6 || isNaN(numMeals) || numMeals % 1 !== 0) {
+ numMeals = Number(input.question("Input must be between 1-6, only whole number: "));
+}
   return numMeals;
 }
 
